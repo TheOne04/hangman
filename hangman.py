02752d1897fb84518eb17db61b6b500778e5e072
TheOne04 <64768475+TheOne04@users.noreply.github.com>
2024-09-15 11:07:22 +0530

    Add logic and function to check for victory

--- hangman.py
+++ hangman.py
@@ -7,9 +7,21 @@ def main():
     wrong_attempts = 0
 
     while True:
+        if wrong_attempts == 7:
+            print_hangman(wrong_attempts)
+            print("Game Over")
+            break
         print_hangman(wrong_attempts)
         print_blanks(word, attempts)
         letter = ask_letter(attempts)
+        if letter not in attempts:
+            attempts.append(letter)
+            if letter not in word:
+                wrong_attempts += 1
+        if guessed_word(word, attempts):
+            print_hangman(wrong_attempts)
+            print("You win!")
+            break
 
     # Loop until hangman complete or word guessed
     # Choose random word
@@ -132,6 +144,13 @@ def ask_letter(attempts):
         letter = input("Guess: ")
         if letter.isalpha() and len(letter) == 1 and letter not in attempts:
             return letter
+
+
+def guessed_word(word, attempts):
+    for letter in set(word):
+        if letter not in attempts:
+            return False
+    return True
 
 
 main()
